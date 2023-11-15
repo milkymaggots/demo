b22d5a6c185d1c038c641ffe14cb02500db862d5
Maksim Damaskanau <maximdomoskanov@gmail.com>
2023-11-15 13:02:26 +0100

    update

---
+++ gitlab_local/run.sh
@@ -0,0 +1,8 @@
+
+
+# GL Runner
+# gitlab-runner (cli)
+
+# Pswd
+sudo docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password # yEHVqM7H2LNIZ0jxrjQhiB30HFqe+KMLvwvopMrDi/w=
+# sudo docker exec -it demo-web-1 grep 'Password:' /etc/gitlab/initial_root_password
