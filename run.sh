6dcb6858a55eb42e9448ff3af070ef5ec0b68930
Max Damaskanau <maximdomoskanov@gmail.com>
2023-11-15 16:46:38 +0100

    update

--- run.sh
+++ run.sh
@@ -7,50 +7,57 @@ drmsudo() { sudo docker stop $(sudo docker ps -a -q); sudo docker rm $(sudo dock
 
 # sudo
 # 
-# sudo
-# --publish 443:443 \
-  # --publish 22:22 \
-export GITLAB_HOME=~/gitlab
-docker run --detach \
-  --hostname gitlab.example.com \
-  --publish 8080:80 \
-  --name gitlab \
-  --restart always \
-  --volume $GITLAB_HOME/config:/etc/gitlab:Z \
-  --volume $GITLAB_HOME/logs:/var/log/gitlab:Z \
-  --volume $GITLAB_HOME/data:/var/opt/gitlab:Z \
-  --shm-size 256m \
-  gitlab/gitlab-ee:latest
+
 
 # Pswd
-# - root
-docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password
+GL_CNTR=gitlab # demo-web-1
+sudo docker exec -it $GL_CNTR grep 'Password:' /etc/gitlab/initial_root_password # OjzvfG44jYTv4aLO1m4A1xISeoNb4OrdvQ2JJkZYe6s=
 
-# Pswd
-sudo docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password # OjzvfG44jYTv4aLO1m4A1xISeoNb4OrdvQ2JJkZYe6s=
-# sudo docker exec -it demo-web-1 grep 'Password:' /etc/gitlab/initial_root_password
+# Network
 
-
+sus systemctl restart systemd-resolved
+# - mask
+sudo systemctl mask systemd-resolved
 
 # GL Runner
 # gitlab-runner (cli)
 
-# # # Runner local
+# Runner
+
+# - register
 gitlab-runner register \
   --url http://localhost:8080 \
   --token glrt-kcGyuAoNsajnf4szz5bY
 
+# - run
+gitlab-runner run
 
-# - config
+# - config view
 cat .gitlab-runner/config.toml
 
-# # # Jimmy
-gpjimmy() {
-  ssh-agent bash -c 'ssh-add ~/.ssh/max_from_jimmy; git push origin main';
-	# git remote set-url origin git@gitlab.com:jimmy-group1/s4e.git
-}
 
-# - git
+# Git
 # - clone
 ssh-agent bash -c 'ssh-add ~/.ssh/max_from_jimmy; git clone -b s4e-19/run-be-locally git@gitlab.com:jimmy-group1/s4e-be.git'
+# - push
+ssh-agent bash -c 'ssh-add ~/.ssh/max_from_jimmy; git push origin main';
+# - remote set
+# git remote set-url origin git@gitlab.com:jimmy-group1/s4e.git
 
+# Docker
+RUN_DOCKER() {
+    # sudo
+  # --publish 443:443 \
+    # --publish 22:22 \
+  export GITLAB_HOME=~/gitlab
+  docker run --detach \
+    --hostname gitlab.example.com \
+    --publish 8080:80 \
+    --name gitlab \
+    --restart always \
+    --volume $GITLAB_HOME/config:/etc/gitlab:Z \
+    --volume $GITLAB_HOME/logs:/var/log/gitlab:Z \
+    --volume $GITLAB_HOME/data:/var/opt/gitlab:Z \
+    --shm-size 256m \
+    gitlab/gitlab-ee:latest
+}
